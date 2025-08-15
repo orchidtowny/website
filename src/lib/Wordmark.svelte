8242dc4de0e8e61c14e8f1aff7cfca0db8201022
ihateblueb <ihateblueb@proton.me>
2025-08-14 23:04:40 -0400

    init

---
+++ src/lib/Wordmark.svelte
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<div class="wordmark">
+    <img src="logo.png" alt="The Orchid logo, an O with an orchid inside of it" />
+    <span>Orchid</span>
+</div>
+
+<style>
+    .wordmark {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 10px;
+    }
+
+    .wordmark img {
+        height: 32px;
+        width: 32px;
+    }
+
+    .wordmark span {
+        font-family: Commissioner, sans-serif;
+        font-feature-settings: normal;
+        font-size: 1.4rem;
+        font-weight: 700;
+        color: var(--tx-1);
+    }
+</style>
